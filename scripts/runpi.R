242b5fea3dd53ce42419ceffcf8386468c72b8a9
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2014-12-05 23:26:39 -0500

    Script changes to reflect inversion changes.

--- scripts/runpi.R
+++ scripts/runpi.R
@@ -2,29 +2,18 @@
 
 args <- commandArgs(trailingOnly=TRUE)
 jrsd <- as.numeric(args[1])
-species <- args[2]
-precarg <- as.numeric(args[3])
-if(precarg){
-        precision <- "sp"
-} else {
-        precision <- "pwl"
-}
-rearg <- args[4]
-initarg <- args[5]
-ngibbs <- as.numeric(args[6])
-runid <- args[7]
-folder <- args[8]
-filename <- sprintf("run_results/%s/%s_%g_%s_%s_%s_%s.dat", folder, species, jrsd, precision, rearg, initarg, runid)
+ngibbs <- as.numeric(args[2])
+runid <- args[3]
+folder <- args[4]
+filename <- sprintf("run_results/%s/%s_%g_hre_%s.dat", folder, species, jrsd, runid)
 dir.create(sprintf("run_results/%s", folder), showWarnings = FALSE)
 
 source("inv_bayes.R")
 source("specdataproc.R")
 
-smat <- specmatrix(species)
+smat <- specmatrix("All")
 
 pinvbayes(smat, ngibbs=ngibbs, JumpRSD=jrsd, fname=filename,
-          local.store=FALSE, 
-          single.precision=precarg, 
-          random.effects=rearg,
-          inits=initarg,
-          ar.step=100)
+          inits='random',
+          ar.step=100,
+          notifications=FALSE)
