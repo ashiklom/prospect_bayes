1a5fc096e249a5882e3f910a81a2b3360c89e6b4
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2015-03-27 17:57:23 -0400

    Set color scale to red-blue.
    TODO:
    Increase font size on axes
    Remove color bar

--- Figures/prospect4_sensitivity.R
+++ Figures/prospect4_sensitivity.R
@@ -4,49 +4,34 @@ library(reshape2)
 library(PEcAnRTM)
 
 data(prospect4)
+vars <- c("N", "Cab", "Cw", "Cm")
 wl <- 400:2500
-plot(wl, P4data[,1], type='l',
-     xlab = "Wavelength",
-     ylab = "Relative absorption",
-     main = "Chlorophyll absorption features")
-plot(wl, P4data[,2], type='l',
-     xlab = "Wavelength",
-     ylab = "Relative absorption",
-     main = "Water absorption features")
-plot(wl, P4data[,3], type='l',
-     xlab = "Wavelength",
-     ylab = "Relative absorption",
-     main = "Dry matter absorption features")
+ns <- 20
+minmax <- list(N = c(1,3),
+               Cab = c(10,70),
+               Cw = c(1e-5, 1e-2),
+               Cm = c(1e-6, 1e-2))
 
-ns <- 30
-N <- seq(1, 3, length.out = ns)
-Ns <- sapply(N, function(x) prospect4(c(x, 30, 0.01, 0.01)))
-Cab <- seq(10, 70, length.out = ns)
-Cabs <- sapply(Cab, function(x) prospect4(c(1.4, x, 0.01, 0.01)))
-Cw <- seq(1e-5, 1e-2, length.out = ns)
-Cws <- sapply(Cw, function(x) prospect4(c(1.4, 30, x, 0.01)))
-Cm <- seq(1e-6, 1e-2, length.out = ns)
-Cms <- sapply(Cm, function(x) prospect4(c(1.4, 30, 0.01, x)))
+seq.log <- function(x) seq(x[1], x[2], length.out = ns)
+seq.list <- lapply(minmax, seq.log)
 
-color <- scale_color_continuous(low="red", high="green")
-Nsr <- melt(Ns)
-Nsr$Var2 <- rep(N, each=2101)
-Np <- ggplot(Nsr) + aes(x=Var1+399, y = value, col=Var2) + geom_line() + 
-        xlab('Wavelength') + ylab('Reflectance') + color
-Cabsr <- melt(Cabs)
-Cabsr$Var2 <- rep(Cab, each=2101)
-Cabp <- ggplot(Cabsr) + aes(x=Var1+399, y = value, col=Var2) + geom_line() + 
-        xlab('Wavelength') + ylab('Reflectance') + color
-Cwsr <- melt(Cws)
-Cwsr$Var2 <- rep(Cw, each=2101)
-Cwp <- ggplot(Cwsr) + aes(x=Var1+399, y = value, col=Var2) + geom_line() + 
-        xlab('Wavelength') + ylab('Reflectance') + color
-Cmsr <- melt(Cms)
-Cmsr$Var2 <- rep(Cm, each=2101)
-Cmp <- ggplot(Cmsr) + aes(x=Var1+399, y = value, col=Var2) + geom_line() + 
-        xlab('Wavelength') + ylab('Reflectance') + color
+Ns <- sapply(seq.list[["N"]], function(x) prospect4(c(x, 30, 0.01, 0.01)))
+Cabs <- sapply(seq.list[["Cab"]], function(x) prospect4(c(1.4, x, 0.01, 0.01)))
+Cws <- sapply(seq.list[["Cw"]], function(x) prospect4(c(1.4, 30, x, 0.01)))
+Cms <- sapply(seq.list[["Cm"]], function(x) prospect4(c(1.4, 30, 0.01, x)))
+sens.spec <- list(N = Ns, Cab = Cabs, Cw = Cws, Cm = Cms)
 
-plot(Np)
-plot(Cabp)
-plot(Cwp)
-plot(Cmp)
+sens.plot <- ggplot() +
+        aes(x = Var1 + 399, y = value, col = Var2, group = Var2) + 
+        geom_line() +
+        scale_color_continuous(low="red", high="blue", trans="sqrt") +
+        xlab("Wavelength") + 
+        ylab("Reflectance")
+
+p <- lapply(vars, function(x) {
+        m <- melt(sens.spec[[x]])
+        m$Var2 <- rep(seq.list[[x]], each=2101)
+        sens.plot %+% m
+})
+
+for(pp in p) plot(pp)
