a28b33579b8afb2c0ef2a3b3fa4075c4cff79e7a
Alexey Shiklomanov <ashiklom@geo.bu.edu>
2015-04-14 16:11:32 -0400

    Output processing

--- process_output.R
+++ process_output.R
@@ -4,3 +4,9 @@ library(coda)
 
 fft.path <- "run_results/FFT_individuals_0323/"
 fft.list <- list.files(fft.path)
+
+bt.list <- list()
+for(f in fft.list){
+        fname <- sprintf("%s%s", fft.path, f)
+        infile <- fread(fname, header=TRUE)
+        b <- infile[80000:100000,,with=FALSE]
