9f30d101023c406301a3a896bb73a5ba00172a28
Alexey Shiklomanov (Arch VM) <alexey.shiklomanov@gmail.com>
2015-07-23 21:31:43 -0400

    Fixed sensor inversion. Added load all script

---
+++ sensor-manuscript/load.all.R
@@ -0,0 +1,48 @@
+#' Script to load all runs
+
+parnames <- c("N", "Cab", "Car", "Cw", "Cm", "residual")
+
+results.path <- "results"
+flist.full <- list.files(results.path)
+results.list <- list()
+for(f in flist.full){
+# Extract data
+    load(f)
+    f.name <- gsub("(.*)[.]RData", "\\1", f)
+    f.list <- get(f.name)
+    rm(list=f.name)
+    sensor <- f.list$sensor
+    samples <- f.list$samples
+    spectra <- f.list$spectra
+
+# Burn in and thin
+    ngibbs <- nrow(samples)
+    burnin <- floor(ngibbs/2)
+    thin <- (ngibbs - burnin) / 5000
+    bt <- seq(burnin, ngibbs, by=thin)
+    samples.sub <- samples[bt,]
+    
+# Calculate summary statistics
+    mu <- colMeans(samples.sub)
+    names(mu) <- sprintf("%s.mu", parnames)
+    sigma <- apply(samples.sub, 2, sd)
+    names(sigma) <- sprintf("%s.sigma", parnames)
+    q25 <- apply(samples.sub, 2, quantile, 0.025)
+    names(q25) <- sprintf("%s.q25", parnames)
+    med <- apply(samples.sub, 2, median)
+    names(med) <- sprintf("%s.med", parnames)
+    q975 <- apply(samples.sub, 2, quantile, 0.975)
+    names(q975) <- sprintf("%s.q975", parnames)
+    dat.row <- c(list(spectra = spectra, 
+                      sensor = sensor,
+                      fname = f.name),
+                 as.list(mu), as.list(sigma),
+                 as.list(q25), as.list(med),
+                 as.list(q975))
+    results.list[[f]] <- dat.row
+}
+
+#' Combine into data.table
+library(data.table)
+fft.dat <- do.call(rbindlist, list(results.list)) 
+save(fft.dat, file="fft.dat.RData")
