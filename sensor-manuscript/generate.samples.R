bdac0d7faf78fadb3efa7051750533a4b15e3b25
Alexey Shiklomanov (Lenovo Lubuntu VM) <alexey.shiklomanov@gmail.com>
2015-07-30 11:42:38 -0400

    Fixed typo in full.mat dimension

--- sensor-manuscript/generate.samples.R
+++ sensor-manuscript/generate.samples.R
@@ -7,7 +7,7 @@ load("../data/FFT.processed.RData")
 vars <- c("N.mu", "Cab.mu", "Car.mu", "Cw.mu", "Cm.mu") 
 fft.sub <- fft.f[!is.na(N.mu)][sensor=="identity"][,vars,with=F]
 full.mat <- as.matrix(fft.sub)
-full.mat <- full.mat[sample(1:nrow(full.mat))]
+full.mat <- full.mat[sample(1:nrow(full.mat)), ]
 #full.mat <- fftmat[sample(1:nrow(fftmat), 300, replace=FALSE),]
 
 # Write submission script
