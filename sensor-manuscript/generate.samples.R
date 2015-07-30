7fe6022cf1f75c89e04283e6ad89413e71bc8461
Alexey Shiklomanov (scc) <ashiklom@bu.edu>
2015-07-30 11:40:00 -0400

    Altered sample generation. Sample full FFT database

--- sensor-manuscript/generate.samples.R
+++ sensor-manuscript/generate.samples.R
@@ -5,20 +5,21 @@ library(data.table)
 library(PEcAnRTM)
 load("../data/FFT.processed.RData")
 vars <- c("N.mu", "Cab.mu", "Car.mu", "Cw.mu", "Cm.mu") 
-fft.sub <- fft.f[!is.na(N.mu)][N.mu < 3][sensor=="identity"][,vars,with=F]
-fftmat <- as.matrix(fft.sub)
-full.mat <- fftmat[sample(1:nrow(fftmat), 300, replace=TRUE),]
+fft.sub <- fft.f[!is.na(N.mu)][sensor=="identity"][,vars,with=F]
+full.mat <- as.matrix(fft.sub)
+full.mat <- full.mat[sample(1:nrow(full.mat))]
+#full.mat <- fftmat[sample(1:nrow(fftmat), 300, replace=FALSE),]
 
 # Write submission script
 n.sensor <- length(sensor.list)
-N <- rep(full.mat[,1], n.sensor)
-Cab <- rep(full.mat[,2], n.sensor)
-Car <- rep(full.mat[,3], n.sensor)
-Cw <- rep(full.mat[,4], n.sensor)
-Cm <- rep(full.mat[,5], n.sensor)
-sensor <- rep(sensor.list, each=nrow(full.mat))
+N <- rep(full.mat[,1], each=n.sensor)
+Cab <- rep(full.mat[,2], each=n.sensor)
+Car <- rep(full.mat[,3], each=n.sensor)
+Cw <- rep(full.mat[,4], each=n.sensor)
+Cm <- rep(full.mat[,5], each=n.sensor)
+sensor <- rep(sensor.list, nrow(full.mat))
 ngibbs <- 50000
-runname <- sprintf("%s.%d", sensor, rep(1:nrow(full.mat), n.sensor))
+runname <- sprintf("%s.%d", sensor, rep(1:nrow(full.mat), each=n.sensor))
 submit.string <- sprintf('qsub -V -v N=%f,Cab=%f,Car=%f,Cw=%f,Cm=%f,sensor=%s,ngibbs=%d,runname=%s -N "%s" submit.simulated.qsub',
                          N, Cab, Car, Cw, Cm, sensor, ngibbs, runname, runname)
 
