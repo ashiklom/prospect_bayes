75170ff7ff85a9355ec76d3189055112362601ba
Alexey Shiklomanov (LVM64) <alexey.shiklomanov@gmail.com>
2015-08-27 16:55:11 -0400

    Documentation for sensor error plot

--- sensor-manuscript/results.sensor-error-plot.R
+++ sensor-manuscript/results.sensor-error-plot.R
@@ -1,11 +1,40 @@
-# Big plot of sensor error
+#' ---
+#' title: Sensor error plot
+#' author: Alexey Shiklomanov
+#' output_format: html_document
+#' ---
+
+#' # Introduction
+#' This script generates a large matrix of scatterplots that show the ability 
+#' of each sensor to accurately retrieve parameters from the simulations. 
+
+#' # Setup
+#' First, we load dependencies and data. The PEcAnRTM package is used only for 
+#' pre-defined sensor information. The remaining packages are used to generate 
+#' the plots. For details on how the data file is generated, see the 
+#' `load.sim.R` file. 
+
 library(PEcAnRTM)
 library(ggplot2)
 library(gridExtra)
 library(grid)
 load("../data/simulation.samp.dat.RData")
 
+#' To facilitate plot organization, we convert the representation of the sensor 
+#' from character to factor ordered based on PEcAnRTM's `sensor.list`.
 simulation.dat[, sensor := factor(sensor, levels=sensor.list)]
+
+#' # Plot specifications
+#' The following lines provide graphical preferences for the plot. The `no.x` 
+#' theme is an additional set of parameters that removes x axis labels. The 
+#' `gen.plot` object is a generalized `ggplot` object describing the data's 
+#' presentation, but without any data present (subsequent lines add this data 
+#' in turn for each parameter). For each parameter (rows) and sensor (columns, 
+#' in order of decreasing spectral resolution), this script draws one 
+#' scatterplot of true parameter values (x) vs mean inversion estimates (y) 
+#' with a dashed red one-to-one line. Specifically, each parameter (row) is 
+#' created as a separate plot, and these plots are then stacked on top of each 
+#' other using the `grid.arrange` function.
 
 gen.theme <- theme_bw() + 
     theme(axis.text = element_text(size=5),
@@ -15,6 +44,7 @@ gen.plot <- ggplot(simulation.dat) +
     facet_grid(.~sensor) + geom_point(size=1) +
     geom_abline(linetype="dashed", color="red") +
     gen.theme
+
 N.plot <- gen.plot + aes(x=N, y=N.mu) + ylab("N") + no.x
 Cab.plot <- gen.plot + aes(x=Cab, y=Cab.mu) + ylab("Cab") + no.x
 Car.plot <- gen.plot + aes(x=Car, y=Car.mu) + ylab("Car") + no.x
