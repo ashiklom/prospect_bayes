2f9cf1ed538b932ba83e899bda4a2870c5d7df17
Alexey Shiklomanov (scc) <ashiklom@bu.edu>
2015-09-03 10:57:54 -0400

    Documentation for load.sim.R

--- sensor-manuscript/load.sim.R
+++ sensor-manuscript/load.sim.R
@@ -1,17 +1,32 @@
-#' Script to load all runs
+#' ---
+#' title: Load results of simulation experiment
+#' author: Alexey Shiklomanov
+#' output_format: html_document
+#' ---
+
+#' The script used to perform inversions on the BU cluster returns an RData 
+#' file containing a list of the matrix of MCMC samples, the name of the 
+#' sensor, the ID of the run, and pre-computed summary statistics. This script 
+#' loops over the list of all simulation output files, loads each, extracts the 
+#' samples, computes the relevant summary statistics, and adds the results to a 
+#' list that is then melded into a `data.table` and saved.
+
 library(PEcAnRTM)
 results.path <- "results-simulation"
 flist.full <- list.files(results.path, ".*.RData")
 results.list <- list()
 for(f in flist.full){
-# Extract data
     print(f)
     dat.row <- load.from.name(f, filepath=results.path)
-    dat.row[c("noise", "obs.raw", "samples")] <- NULL
-    results.list[[f]] <- dat.row
+    samples <- dat.row$samples
+    samples.bt <- burnin.thin(samples)
+    samples.summary <- summary.simple(samples.bt)
+    dat.sub <- dat.row[c("sensor", "fname")]
+    results.list[[f]] <- c(dat.sub, samples.summary)
 }
 
-#' Combine into data.table
+#' This step uses the `rbindlist` function from the `data.table` package for 
+#' very efficient row binding of multiple list objects. 
 library(data.table)
 simulation.dat <- do.call(rbindlist, list(results.list)) 
 save(simulation.dat, file="../data/simulation.samp.dat.RData")
